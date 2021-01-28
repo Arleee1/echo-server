0fb274a93171cf9bd8882077c762d69bfd45fe2d
Arleee1 <61568556+Arleee1@users.noreply.github.com>
2021-01-28 12:10:56 -0500

    Added main node file

---
+++ echo_server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+var server = http.createServer(handler);
+var args = process.argv;
+var server_port_arg = parseInt(args[2])
+var server_port = isNaN(server_port_arg) ? 1000 : server_port_arg;
+server.listen(server_port);
+console.log(`Server running on port: ${server_port}`)
+function handler(req, res) {
+  console.log(req.url);
+  res.end(req.url);
+}

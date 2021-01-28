3d3c2b100814b278a4229bbdd40104524aaf7dec
Arleee1 <61568556+Arleee1@users.noreply.github.com>
2021-01-28 12:11:47 -0500

    Added semicolon

--- echo_server.js
+++ echo_server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 var server = http.createServer(handler);
 var args = process.argv;
-var server_port_arg = parseInt(args[2])
+var server_port_arg = parseInt(args[2]);
 var server_port = isNaN(server_port_arg) ? 1000 : server_port_arg;
 server.listen(server_port);
 console.log(`Server running on port: ${server_port}`)

b9b5059f3d208b152933a86c8c1e63e993ef0e50
Arleee1 <61568556+Arleee1@users.noreply.github.com>
2021-01-29 17:59:47 -0500

    Added comma

--- echo_server.js
+++ echo_server.js
@@ -4,7 +4,7 @@ var args = process.argv;
 var server_port_arg = parseInt(args[2]);
 var server_port = isNaN(server_port_arg) ? 1000 : server_port_arg;
 server.listen(server_port);
-console.log(`Server running on port: ${server_port}`)
+console.log(`Server running on port: ${server_port}`);
 function handler(req, res) {
   console.log(req.url);
   res.end(req.url);
